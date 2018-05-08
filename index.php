38034631ff334b90b8e3376ff96412cffb7aff17
toylaa <tcmcd1337@gmail.com>
2018-05-08 16:04:55 -0400

    NavBar Prototyping
    Created dir /php && /styles to hold reuseable components.

--- index.php
+++ index.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
+
+<link rel="stylesheet" type="text/css" href="styles/nav.css">
 <html>
 <head>
 	<title>Unified Disposal Services - Home for all things trash related</title>
 </head>
 <body>
-
-
+ <?php require_once 'php/nav.php'; ?>
+<!--h1>This is a test </h1-->
 
 
 </body>
---
+++ php/nav.php
@@ -0,0 +1,60 @@
+<!-- NavBar Style via https://codepen.io/maheshambure21/pen/QwXaRw -->
+
+<link href='https://fonts.googleapis.com/css?family=Roboto:500,900,100,300,700,400' rel='stylesheet' type='text/css'>
+
+
+<!--section style="background: #2ecc71; color: rgba(0, 0, 0, 0.5);"-->
+  
+  <nav class="fill">
+    <ul>
+      <li><a href="#">Services</a></li>
+      <li><a href="#">Equipment</a></li>
+      <li><a href="#">About</a></li>
+      
+      <li><a href="#">Nice staff</a></li>
+    </ul>
+  </nav>
+
+  <h2>Stroke-Fill navbar</h2>
+  <br/>
+
+<!--/section-->
+
+<section style="background: #e74c3c; color: #fff;">
+  <h2>Underline Stroke</h2>
+  <nav class="stroke">
+    <ul>
+      <li><a href="#">Home</a></li>
+      <li><a href="#">About</a></li>
+      <li><a href="#">Downloads</a></li>
+      <li><a href="#">More</a></li>
+      <li><a href="#">Nice staff</a></li>
+    </ul>
+  </nav>
+</section>
+
+<section style="background: #34495e; color: rgba(255, 255, 255, 0.5);">
+  <h2>Circle Fill</h2>
+  <nav class="circle">
+    <ul>
+      <li><a href="#">Home</a></li>
+      <li><a href="#">About</a></li>
+      <li><a href="#">Downloads</a></li>
+      <li><a href="#">More</a></li>
+      <li><a href="#">Nice staff</a></li>
+    </ul>
+  </nav>
+</section>
+<section style="background: #f1c40f; color: rgba(0, 0, 0, 0.5);">
+  <h2>Shift</h2>
+  <nav class="shift">
+    <ul>
+      <li><a href="#">Home</a></li>
+      <li><a href="#">About</a></li>
+      <li><a href="#">Downloads</a></li>
+      <li><a href="#">More</a></li>
+      <li><a href="#">Nice staff</a></li>
+    </ul>
+  </nav>
+</section>  
+
