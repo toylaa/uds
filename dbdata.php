e443841208fe38fc0c6b9adac3d23951cdb00ed5
jojijac0b <jojijacob22@gmail.com>
2018-05-17 17:57:10 -0700

    seeing if dbdata displays all data from db 7

--- dbdata.php
+++ dbdata.php
@@ -4,7 +4,7 @@ include ('db.php');
 
   		echo 'This is Index Page';
   		$sql = 'SELECT * FROM uds_first_try.posts';
-  		$stmt = $pdo->prepare($sql);
+  		$stmt = $pdo->query($sql);
   		$stmt->execute();
   		$rowCount = $stmt->rowCount();
   		$details = $stmt->fetch();
