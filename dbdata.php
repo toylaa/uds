db123d17c3df1b121ad60bd941ca44f2d553b92e
jojijac0b <jojijacob22@gmail.com>
2018-05-17 18:22:57 -0700

    seeing if dbdata displays all data from db 16

--- dbdata.php
+++ dbdata.php
@@ -6,7 +6,7 @@ $password = "dd4d84b264bff6405448085e4fbc365d402d5cd8f3f3b804020682af42b8df3d";
 $dbname = "ddgdjlnmve8vcc";
 $port = "5432";
 
-$con = pg_connect("host=$host dbname=$db user=$user password=$pass" port=$port)
+$con = pg_connect("host=$host dbname=$db user=$user password=$pass")
     or die ("Could not connect to server\n"); 
 
 $query = "SELECT * FROM uds_first_try.posts"; 
