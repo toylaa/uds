5ff371d4690427520012ce726171e1931c1a2076
jojijac0b <jojijacob22@gmail.com>
2018-05-17 22:14:24 -0700

    getting text from searchbar text field 21

--- dbdata.php
+++ dbdata.php
@@ -24,7 +24,7 @@ if (!$link)
 $searchText = $_SESSION['searchText'];
 
 
-$query = 'select * from uds_first_try.posts where name ilike \'.$searchText.\' or description ilike \'.$searchText.\'';
+$query = 'select * from uds_first_try.posts where name ilike \'%'.$searchText.'%\' or description ilike \'%'.$searchText.'%\'';
 
 $result = pg_query($query);
 
@@ -58,5 +58,5 @@ pg_free_result($result);
 
 echo '</table></body></html>';
 echo $_SESSION['searchText'];
-echo 'select * from uds_first_try.posts where name ilike \''.$searchText.'\' or description ilike \''.$searchText.'\'';
+echo 'select * from uds_first_try.posts where name ilike \'%'.$searchText.'%\' or description ilike \'%'.$searchText.'%\'';
 ?>
