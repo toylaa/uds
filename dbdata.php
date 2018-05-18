fdf6fe5007e96144b322b125ef17e725003ef91a
jojijac0b <jojijacob22@gmail.com>
2018-05-17 18:41:04 -0700

    seeing if dbdata displays all data from db 21

--- dbdata.php
+++ dbdata.php
@@ -1,13 +1,13 @@
 
 <?php
 
-$host = '127.0.0.1';
-$port = '5432';
-$database = 'sample';
-$user = 'testuser';
-$password = 'testpass';
+$host = "ec2-54-243-137-182.compute-1.amazonaws.com";
+$user = "qkfgniqxbkiaju";
+$password = "dd4d84b264bff6405448085e4fbc365d402d5cd8f3f3b804020682af42b8df3d";
+$dbname = "ddgdjlnmve8vcc";
+$port = "5432";
 
-$connectString = 'host=' . $host . ' port=' . $port . ' dbname=' . $database . 
+$connectString = 'host=' . $host . ' port=' . $port . ' dbname=' . $dbname . 
 	' user=' . $user . ' password=' . $password;
 
 
