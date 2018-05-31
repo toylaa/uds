6c061cfbd1bbcddefa14a44285101ef431492506
jojijac0b <jojijacob22@gmail.com>
2018-05-30 20:44:42 -0700

    search results styling

--- index.php
+++ index.php
@@ -24,7 +24,21 @@ session_start();
 
 
 <head>
-	<title>Unified Disposal Services - Home for all things trash related</title>
+	<style>
+table, th, td {
+    border: 1px solid black;
+    border-collapse: collapse;
+}
+th, td {
+    padding: 15px;
+    text-align: left;
+}
+table#t01 {
+    width: 100%;    
+    background-color: #f1f1c1;
+}
+</style>
+	<title>Price Right Disposal - Home for all things trash related</title>
 </head>
 <body>
 
